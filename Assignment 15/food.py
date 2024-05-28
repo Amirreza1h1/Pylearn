eda1394cda98caa2cbdc60e343981d0318c79e6f
Amirreza Hajizadeh <amirreza.haji.0926264710@gmail.com>
2024-05-28 23:06:12 +0430

    food

--- Assignment 15/food.py
+++ Assignment 15/food.py
@@ -3,7 +3,7 @@ import arcade
 
 
 class Food(arcade.Sprite):
-    def __init__(self,game,picture):
+    def __init__(self, game, picture):
         super().__init__(picture)
         self.width = 32
         self.height = 32
@@ -11,26 +11,22 @@ class Food(arcade.Sprite):
         self.center_y = random.randint(10, game.height-10)
         self.change_x = 0
         self.change_y = 0
-        self.score=None
+        self.score = None
 
 
 class Apple(Food):
     def __init__(self, game):
-        super().__init__(game,'apple.png')
-        self.score=1
-        # self.width = 32
-        # self.height = 32
-        # self.center_x = random.randint(10, game.width-10)
-        # self.center_y = random.randint(10, game.height-10)
-        # self.change_x = 0
-        # self.change_y = 0
+        super().__init__(game, 'apple.png')
+        self.score = 1
+
 
 class Banana(Food):
-    def __init__(self,game):
-        super().__init__(game,'banana.png')
-        self.score=2
+    def __init__(self, game):
+        super().__init__(game, 'banana.png')
+        self.score = 2
+
 
 class Poo(Food):
-    def __init__(self,game):
-        super().__init__(game,'poo.png')
-        self.score=-1
+    def __init__(self, game):
+        super().__init__(game, 'poo.png')
+        self.score = -1
