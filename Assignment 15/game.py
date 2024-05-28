1393f9855b074e3ac22a0efa634f61e63dd1f26b
Amirreza Hajizadeh <amirreza.haji.0926264710@gmail.com>
2024-05-28 23:06:36 +0430

    update

--- Assignment 15/game.py
+++ Assignment 15/game.py
@@ -2,26 +2,38 @@ import arcade
 import food
 import snake
 
+WINDOW_SIZE_WIDTH = 500
+WINDOW_SIZE_HEIGHT = 500
+
 
 class Game(arcade.Window):
     def __init__(self):
-        super().__init__(width=500, height=500, title="Snake 🐍 V1")
+        super().__init__(WINDOW_SIZE_WIDTH, WINDOW_SIZE_HEIGHT, title="Snake 🐍 V1")
         arcade.set_background_color(arcade.color.KHAKI)
-
+        self.game_over = False
         self.snake = snake.Snake(self)
         self.food_1 = food.Apple(self)
         self.food_2 = food.Banana(self)
-        self.food_3=food.Poo(self)
-
+        self.food_3 = food.Poo(self)
 
     def on_draw(self):
         arcade.start_render()
-        self.snake.draw()
-        arcade.draw_text("score: "+f"{self.snake.score}", self.width-50, self.height//50, arcade.color.BLACK, font_size=12, anchor_x="center")
+        if self.game_over:
+            arcade.set_background_color(arcade.color.BLACK)
+            arcade.draw_text("): Game Over :(", self.width // 2, self.height // 2,
+                             color=arcade.color.RED, font_size=70, anchor_x="center")
+            arcade.draw_text("Your score: "+f"{self.snake.score}", self.width//2, self.height //
+                             3, arcade.color.WHITE, font_size=30, anchor_x="center")
+            # time.sleep(5)
+            # exit(0)
+        else:
+            self.snake.draw()
+            arcade.draw_text("score: "+f"{self.snake.score}", self.width-50,
+                             self.height//50, arcade.color.BLACK, font_size=12, anchor_x="center")
 
-        self.food_1.draw()
-        self.food_2.draw()
-        self.food_3.draw()
+            self.food_1.draw()
+            self.food_2.draw()
+            self.food_3.draw()
 
         arcade.finish_render()
 
@@ -40,29 +52,25 @@ class Game(arcade.Window):
             self.snake.change_y = 0
 
     def on_update(self, delta_time: float):
-        self.snake.move()
+        if not self.game_over:
+            self.snake.move()
 
-        if arcade.check_for_collision(self.snake, self.food_1):
-            self.snake.eat(self.food_1)
-            self.food_1 = food.Apple(self)
+            if arcade.check_for_collision(self.snake, self.food_1):
+                self.snake.eat(self.food_1)
+                self.food_1 = food.Apple(self)
 
-        if arcade.check_for_collision(self.snake, self.food_2):
-            self.snake.eat(self.food_2)
-            self.food_2 = food.Banana(self)
+            if arcade.check_for_collision(self.snake, self.food_2):
+                self.snake.eat(self.food_2)
+                self.food_2 = food.Banana(self)
 
-        
-        if arcade.check_for_collision(self.snake, self.food_3):
-            self.snake.eat(self.food_3)
-            self.food_3 = food.Poo(self)
-        
-        #game over needs modify
-        while 1:
-            if arcade.check_for_collision(self.snake,self.snake):
-                print("game over!")
-            if self.snake.center_x-32==0 or self.snake.center_x+32==500:
-                print("game over!")
-            if self.snake.center_y-32==0 or self.snake.center_y+32==500:
-                print("game over!")
-            if self.snake.score==0:
-                print("game over!")
-                
+            if arcade.check_for_collision(self.snake, self.food_3):
+                self.snake.eat(self.food_3)
+                self.food_3 = food.Poo(self)
+
+            if self.snake.center_x <= 0 or self.snake.center_x >= WINDOW_SIZE_WIDTH:
+                self.game_over = True
+                print("Game Over")
+
+            if self.snake.center_y <= 0 or self.snake.center_y >= WINDOW_SIZE_HEIGHT:
+                self.game_over = True
+                print("Game Over")
--- Assignment 15/snake.py
+++ Assignment 15/snake.py
@@ -1,43 +1,57 @@
 import arcade
+import arcade.color
+import arcade.color
 
-snake_color=1
+
 class Snake(arcade.Sprite):
     def __init__(self, game):
         super().__init__()
-        self.width = 32
-        self.height = 32
+        self.width = 28
+        self.height = 28
         self.center_x = game.width//2
         self.center_y = game.height//2
-        self.color = arcade.color.BLUE
         self.change_x = 0
         self.change_y = 0
         self.speed = 4
         self.score = 1
         self.body = []
+        self.body_size = 0
+        self.color1 = arcade.color.RED
+        self.color2 = arcade.color.GREEN
 
     def draw(self):
-        global snake_color
-        arcade.draw_rectangle_filled(
-            self.center_x, self.center_y, self.width, self.height, self.color)
-
-        for part in self.body:
-            if snake_color==1:
+        for i, part in enumerate(self.body):
+            if i % 2 == 0:
                 arcade.draw_rectangle_filled(
-                part['x'], part['y'], self.width, self.height, arcade.color.GREEN)
-                snake_color+=1
+                    part['x'], part['y'], self.width, self.height, self.color2)
             else:
                 arcade.draw_rectangle_filled(
-                part['x'], part['y'], self.width, self.height, arcade.color.RED)
-                snake_color-=1
+                    part['x'], part['y'], self.width, self.height, self.color1)
+            i += 1
+
+        arcade.draw_rectangle_filled(
+            self.center_x, self.center_y, self.width, self.height, arcade.color.BLUE)
 
     def move(self):
-        self.body.append({'x': self.center_x, 'y': self.center_y})
-        if len(self.body) > self.score:
+        if self.change_x != 0:
+            if self.change_x == 1:
+                self.body.append({'x': self.center_x, 'y': self.center_y})
+            if self.change_x == -1:
+                self.body.append({'x': self.center_x, 'y': self.center_y})
+        elif self.change_y != 0:
+            if self.change_y == 1:
+                self.body.append({'x': self.center_x, 'y': self.center_y})
+            if self.change_y == -1:
+                self.body.append({'x': self.center_x, 'y': self.center_y})
+
+        if len(self.body) > self.body_size:
             self.body.pop(0)
+
         self.center_x += self.change_x*self.speed
         self.center_y += self.change_y*self.speed
 
     def eat(self, food):
-        score=food.score
+        score = food.score
         del food
-        self.score+=score
+        self.score += score
+        self.body_size += 1
