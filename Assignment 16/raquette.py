debe92287cb25b0a750f0908763ce497dc339224
Amirreza Hajizadeh <amirreza.haji.0926264710@gmail.com>
2024-06-28 19:42:17 +0430

    update

---
+++ Assignment 16/raquette.py
@@ -0,0 +1,21 @@
+import arcade
+
+class Raquette(arcade.Sprite):
+    def __init__(self,x,y,n,c):
+        super().__init__()
+        self.center_x=x
+        self.center_y=y
+        self.name=n
+        self.color=c
+        self.change_x=0
+        self.change_y=0
+        self.width=10
+        self.height=70
+        self.speed=4
+        self.score=0
+
+    def move(self):
+        ...
+    
+    def draw(self):
+        arcade.draw_rectangle_filled(self.center_x,self.center_y,self.width,self.height,self.color)
