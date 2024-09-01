ff87977a406fb093eb31d06e5aa1058a7a5de5ce
Amirreza Hajizadeh <amirreza.haji.0926264710@gmail.com>
2024-09-01 22:06:39 +0430

    rock paper scissors

--- GUI (Qt)/Assignment 19/rock paper scissors/main.py
+++ GUI (Qt)/Assignment 19/rock paper scissors/main.py
@@ -1,4 +1,12 @@
 import sys
+import random
 from functools import partial
-from PySide6.QtWidgets import QApplication, QMessageBox, QRadioButton
+from PySide6.QtWidgets import QApplication, QMessageBox
 from PySide6.QtUiTools import QUiLoader
+
+loader = QUiLoader()
+my_app = QApplication(sys.argv)
+my_window = loader.load("main.ui")
+
+my_window.show()
+my_app.exec()
