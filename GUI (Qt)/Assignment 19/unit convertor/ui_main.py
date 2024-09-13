b967a2fe195e5f8204c511dc0f928bdbd8767b93
Amirreza Hajizadeh <amirreza.haji.0926264710@gmail.com>
2024-09-13 22:14:35 +0430

    update

--- GUI (Qt)/Assignment 19/unit convertor/ui_main.py
+++ GUI (Qt)/Assignment 19/unit convertor/ui_main.py
@@ -15,14 +15,14 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QLineEdit, QMainWindow,
-    QPushButton, QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QGridLayout, QLineEdit,
+    QMainWindow, QPushButton, QSizePolicy, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(774, 551)
+        MainWindow.resize(601, 531)
         MainWindow.setStyleSheet(u"\n"
 "QPushButton {\n"
 "	border: 2px solid #8f8f91;\n"
@@ -63,81 +63,97 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout = QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.pushButton_2 = QPushButton(self.centralwidget)
-        self.pushButton_2.setObjectName(u"pushButton_2")
-        sizePolicy = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+        self.Type_box = QComboBox(self.centralwidget)
+        self.Type_box.addItem("")
+        self.Type_box.addItem("")
+        self.Type_box.addItem("")
+        self.Type_box.addItem("")
+        self.Type_box.setObjectName(u"Type_box")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Maximum)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.pushButton_2.sizePolicy().hasHeightForWidth())
-        self.pushButton_2.setSizePolicy(sizePolicy)
+        sizePolicy.setHeightForWidth(self.Type_box.sizePolicy().hasHeightForWidth())
+        self.Type_box.setSizePolicy(sizePolicy)
 
-        self.gridLayout.addWidget(self.pushButton_2, 2, 2, 1, 1)
+        self.gridLayout.addWidget(self.Type_box, 1, 0, 1, 3)
 
-        self.lineEdit = QLineEdit(self.centralwidget)
-        self.lineEdit.setObjectName(u"lineEdit")
-        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+        self.lineEdit_6 = QLineEdit(self.centralwidget)
+        self.lineEdit_6.setObjectName(u"lineEdit_6")
+        sizePolicy.setHeightForWidth(self.lineEdit_6.sizePolicy().hasHeightForWidth())
+        self.lineEdit_6.setSizePolicy(sizePolicy)
+        self.lineEdit_6.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.lineEdit_6.setStyleSheet(u"")
+        self.lineEdit_6.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.lineEdit_6.setReadOnly(True)
+
+        self.gridLayout.addWidget(self.lineEdit_6, 0, 0, 1, 3)
+
+        self.From_box = QComboBox(self.centralwidget)
+        self.From_box.setObjectName(u"From_box")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Maximum)
         sizePolicy1.setHorizontalStretch(0)
         sizePolicy1.setVerticalStretch(0)
-        sizePolicy1.setHeightForWidth(self.lineEdit.sizePolicy().hasHeightForWidth())
-        self.lineEdit.setSizePolicy(sizePolicy1)
+        sizePolicy1.setHeightForWidth(self.From_box.sizePolicy().hasHeightForWidth())
+        self.From_box.setSizePolicy(sizePolicy1)
 
-        self.gridLayout.addWidget(self.lineEdit, 4, 0, 1, 1)
+        self.gridLayout.addWidget(self.From_box, 4, 0, 1, 1)
 
-        self.pushButton = QPushButton(self.centralwidget)
-        self.pushButton.setObjectName(u"pushButton")
-        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.MinimumExpanding)
+        self.lineEdit_5 = QLineEdit(self.centralwidget)
+        self.lineEdit_5.setObjectName(u"lineEdit_5")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed)
         sizePolicy2.setHorizontalStretch(0)
         sizePolicy2.setVerticalStretch(0)
-        sizePolicy2.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
-        self.pushButton.setSizePolicy(sizePolicy2)
+        sizePolicy2.setHeightForWidth(self.lineEdit_5.sizePolicy().hasHeightForWidth())
+        self.lineEdit_5.setSizePolicy(sizePolicy2)
+        self.lineEdit_5.setStyleSheet(u"")
+        self.lineEdit_5.setAlignment(Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignTop)
+        self.lineEdit_5.setReadOnly(True)
 
-        self.gridLayout.addWidget(self.pushButton, 2, 0, 1, 1)
-
-        self.lineEdit_3 = QLineEdit(self.centralwidget)
-        self.lineEdit_3.setObjectName(u"lineEdit_3")
-        self.lineEdit_3.setMaximumSize(QSize(100, 16777215))
-        self.lineEdit_3.setReadOnly(True)
-
-        self.gridLayout.addWidget(self.lineEdit_3, 1, 0, 1, 1)
-
-        self.lineEdit_2 = QLineEdit(self.centralwidget)
-        self.lineEdit_2.setObjectName(u"lineEdit_2")
-
-        self.gridLayout.addWidget(self.lineEdit_2, 4, 2, 1, 1)
+        self.gridLayout.addWidget(self.lineEdit_5, 6, 1, 1, 1, Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignVCenter)
 
         self.lineEdit_4 = QLineEdit(self.centralwidget)
         self.lineEdit_4.setObjectName(u"lineEdit_4")
         self.lineEdit_4.setMaximumSize(QSize(100, 16777215))
         self.lineEdit_4.setReadOnly(True)
 
-        self.gridLayout.addWidget(self.lineEdit_4, 1, 2, 1, 1)
+        self.gridLayout.addWidget(self.lineEdit_4, 3, 2, 1, 1)
 
-        self.lineEdit_5 = QLineEdit(self.centralwidget)
-        self.lineEdit_5.setObjectName(u"lineEdit_5")
-        sizePolicy3 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed)
+        self.lineEdit_3 = QLineEdit(self.centralwidget)
+        self.lineEdit_3.setObjectName(u"lineEdit_3")
+        self.lineEdit_3.setMaximumSize(QSize(100, 16777215))
+        self.lineEdit_3.setReadOnly(True)
+
+        self.gridLayout.addWidget(self.lineEdit_3, 3, 0, 1, 1)
+
+        self.To_output = QLineEdit(self.centralwidget)
+        self.To_output.setObjectName(u"To_output")
+        self.To_output.setReadOnly(True)
+
+        self.gridLayout.addWidget(self.To_output, 6, 2, 1, 1)
+
+        self.To_box = QComboBox(self.centralwidget)
+        self.To_box.setObjectName(u"To_box")
+        sizePolicy1.setHeightForWidth(self.To_box.sizePolicy().hasHeightForWidth())
+        self.To_box.setSizePolicy(sizePolicy1)
+
+        self.gridLayout.addWidget(self.To_box, 4, 2, 1, 1)
+
+        self.From_input = QLineEdit(self.centralwidget)
+        self.From_input.setObjectName(u"From_input")
+        sizePolicy3 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
         sizePolicy3.setHorizontalStretch(0)
         sizePolicy3.setVerticalStretch(0)
-        sizePolicy3.setHeightForWidth(self.lineEdit_5.sizePolicy().hasHeightForWidth())
-        self.lineEdit_5.setSizePolicy(sizePolicy3)
-        self.lineEdit_5.setStyleSheet(u"")
-        self.lineEdit_5.setAlignment(Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignTop)
-        self.lineEdit_5.setReadOnly(True)
+        sizePolicy3.setHeightForWidth(self.From_input.sizePolicy().hasHeightForWidth())
+        self.From_input.setSizePolicy(sizePolicy3)
 
-        self.gridLayout.addWidget(self.lineEdit_5, 4, 1, 1, 1, Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignVCenter)
+        self.gridLayout.addWidget(self.From_input, 6, 0, 1, 1)
 
         self.pushButton_3 = QPushButton(self.centralwidget)
         self.pushButton_3.setObjectName(u"pushButton_3")
         sizePolicy.setHeightForWidth(self.pushButton_3.sizePolicy().hasHeightForWidth())
         self.pushButton_3.setSizePolicy(sizePolicy)
 
-        self.gridLayout.addWidget(self.pushButton_3, 9, 0, 1, 3)
-
-        self.pushButton_4 = QPushButton(self.centralwidget)
-        self.pushButton_4.setObjectName(u"pushButton_4")
-        sizePolicy.setHeightForWidth(self.pushButton_4.sizePolicy().hasHeightForWidth())
-        self.pushButton_4.setSizePolicy(sizePolicy)
-
-        self.gridLayout.addWidget(self.pushButton_4, 0, 0, 1, 3)
+        self.gridLayout.addWidget(self.pushButton_3, 7, 1, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
 
@@ -148,12 +164,15 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Unit Convertor", None))
-        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
-        self.lineEdit_3.setText(QCoreApplication.translate("MainWindow", u"From:", None))
-        self.lineEdit_4.setText(QCoreApplication.translate("MainWindow", u"To:", None))
+        self.Type_box.setItemText(0, QCoreApplication.translate("MainWindow", u"Mass", None))
+        self.Type_box.setItemText(1, QCoreApplication.translate("MainWindow", u"Length", None))
+        self.Type_box.setItemText(2, QCoreApplication.translate("MainWindow", u"Temperature", None))
+        self.Type_box.setItemText(3, QCoreApplication.translate("MainWindow", u"Volume", None))
+
+        self.lineEdit_6.setText(QCoreApplication.translate("MainWindow", u"Convertor", None))
         self.lineEdit_5.setText(QCoreApplication.translate("MainWindow", u"=", None))
+        self.lineEdit_4.setText(QCoreApplication.translate("MainWindow", u"To:", None))
+        self.lineEdit_3.setText(QCoreApplication.translate("MainWindow", u"From:", None))
         self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"Equal", None))
-        self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"Convert", None))
     # retranslateUi
 
