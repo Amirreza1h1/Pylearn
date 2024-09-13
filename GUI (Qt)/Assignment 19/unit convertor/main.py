17b83c3a3886f8bd5b983250681b6bd5516f7a9f
Amirreza Hajizadeh <amirreza.haji.0926264710@gmail.com>
2024-09-13 22:45:41 +0430

    update

--- GUI (Qt)/Assignment 19/unit convertor/main.py
+++ GUI (Qt)/Assignment 19/unit convertor/main.py
@@ -9,6 +9,28 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        measurement_types = {
+            "Mass": ["kg", "g", "mg"],
+            "Length": ["m", "cm", "mm"],
+            "Temperature": ["°C", "K", "°F"],
+            "Volume": ["L", "ml", "t"]
+        }
+        self.From_box.clear()
+        self.To_box.clear()
+        self.update_boxes(self.Type_box.currentText(),measurement_types)
+
+
+    def update_boxes(self,selected_type,measurement_types):
+        self.From_box.clear()
+        self.To_box.clear()
+
+        if selected_type in measurement_types:
+            units = measurement_types[selected_type]
+            self.From_box.addItems(units)
+            self.To_box.addItems(units)
+        else:
+            self.From_box.addItem("")
+            self.To_box.addItem("")
 
 my_app = QApplication(sys.argv)
 main_window = MainWindow()
