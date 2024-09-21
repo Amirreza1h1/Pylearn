51d4b616554046424b482b016abc66945e1d2fbf
Amirreza Hajizadeh <amirreza.haji.0926264710@gmail.com>
2024-09-21 22:12:57 +0330

    update

--- GUI (Qt)/Assignment 19/rock paper scissors/main.py
+++ GUI (Qt)/Assignment 19/rock paper scissors/main.py
@@ -1,5 +1,4 @@
 import sys
-import time
 import random
 from functools import partial
 from PySide6.QtWidgets import QApplication, QMessageBox, QMainWindow
@@ -7,25 +6,49 @@ from ui_main import Ui_MainWindow
 
 
 class MainWindow(QMainWindow):
-    def __init__(self,parent=None):
+    def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        # self.rock.clicked.connect(self.play)
-        # self.paper.clicked.connect(self.play)
-        # self.scissors.clicked.connect(self.play)
+        self.ui.rock.clicked.connect(partial(self.play,1))
+        self.ui.paper.clicked.connect(partial(self.play,2))
+        self.ui.scissors.clicked.connect(partial(self.play,3))
         self.ui.restart.clicked.connect(self.restart)
         self.ui.about.clicked.connect(self.about)
 
-    def restart(self):
-        self.player_status.clear()
-        self.cpu_status.clear()
-        self.result.clear()
-        self.player_status.setText("Player WINs")
-        self.cpu_status.setText("CPU WINs")
+        self.choice={1:"Rock 🪨",
+                     2:"Paper 📄",
+                     3:"Scissors ✂️"}
 
-    def about():
+
+    def ai_play(self):
+        ai_choice=self.choice(random.randint(1,3))
+        return ai_choice
+
+
+    def play(self,user_push):
+        try:
+            user_choice=self.choice(user_push)
+            cpu_choice=self.ai_play()
+            
+
+
+        except ValueError:
+            QMessageBox.warning(self, "Invalid Input",
+                                "Please enter a valid number.")
+
+
+    def restart(self):
+        self.ui.user_win.clear()
+        self.ui.cpu_win.clear()
+        self.ui.cpu_choice.clear()
+        self.ui.user_choice.clear()
+        msg_box = QMessageBox(
+            text="Game restart!")
+        msg_box.exec()
+
+    def about(self):
         msg_box = QMessageBox(
             text="Rock Paper Scissors \n It's a game that: \n rock wins against scissors. \n scissors wins against paper. \n paper wins against rock. \n Let's play!")
         msg_box.exec()
