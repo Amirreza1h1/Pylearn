2652c60e70e648311fa32404e5f090221118c840
Amirreza Hajizadeh <amirreza.haji.0926264710@gmail.com>
2024-10-05 19:03:29 +0330

    random mariage

--- GUI (Qt)/Assignment 20/exercise!/random mariage.py
+++ GUI (Qt)/Assignment 20/exercise!/random mariage.py
@@ -1,4 +1,20 @@
+import random
 
+def mariage(boys,girls):
+    marry=[]
+
+    while True:
+        if len(boys)>0 and len(girls)>0:
+            boy=boys.pop(random.randint(0,len(boys)-1))
+            girl=girls.pop(random.randint(0,len(girls)-1))
+            m=f"({boy},{girl})"
+            marry.append(m)
+        else:
+            break
+
+    return marry
 
 boys = ["mohammad", "sobhan", "abdollah", "kiya", "mahdi", "sajjad", "homan", "arman"]
 girls = ["mahtab", "hane", "harir", "fateme", "kiana", "faezeh", "minoo", "mina", "soghra"]
+
+print(mariage(boys,girls))
