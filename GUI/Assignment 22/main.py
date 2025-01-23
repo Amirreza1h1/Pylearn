5e5138a7028a8fa97d65dfa5cdcc6d02185761c5
Amirreza Hajizadeh <amirreza.haji.0926264710@gmail.com>
2025-01-24 03:16:36 +0330

    main

---
+++ GUI/Assignment 22/main.py
@@ -0,0 +1,20 @@
+import sys
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
+from ui_main import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui=Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+
+if __name__=="__main__":
+    app=QApplication(sys.argv)
+
+    main_window=MainWindow()
+    main_window.show()
+
+    app.exec()
